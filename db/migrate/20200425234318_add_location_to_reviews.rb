27f40ec5193b12b9360ed215699fd15703f3ad87
Rik Lomas <lomas.rik@gmail.com>
2020-04-27 11:34:53 -0400

    Change migration from text_field to text as Heroku is having issues

--- db/migrate/20200425234318_add_location_to_reviews.rb
+++ db/migrate/20200425234318_add_location_to_reviews.rb
@@ -1,7 +1,7 @@
 class AddLocationToReviews < ActiveRecord::Migration[6.0]
   def change
     
-    add_column :reviews, :adress, :text_field
+    add_column :reviews, :adress, :text
     add_column :reviews, :latitude, :float
     add_column :reviews, :longitude, :float
 
